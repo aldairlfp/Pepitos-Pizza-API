76c8bb5e810d852e95d2c6232a53d95eac572e86
aldairlfp <aldairalfonsoperez@gmail.com>
2022-06-22 01:44:42 -0700

    Fixing some things

--- api/poblate.py
+++ api/poblate.py
@@ -2,51 +2,51 @@ from matplotlib.style import available
 from .models import *
 from django.contrib.auth.models import User, Group
 
-@staticmethod
-def poblate():
-    # Amount.objects.create(id=1, amount='Low')
-    # Amount.objects.create(id=2, amount='Meduim')
-    # Amount.objects.create(id=3, amount='High')
+class Poblation(object):
 
-    # Added.objects.create(id=1, name='queso')
-    # Added.objects.create(id=2, name='jamon')
-
-    # addeds = Added.objects.all()
-    # amounts = Amount.objects.all()
-
-    # AmountAdded.objects.create(id=1, added=addeds[0], amount=amounts[0])
-    # AmountAdded.objects.create(id=2, added=addeds[1], amount=amounts[0])
-    # AmountAdded.objects.create(id=3, added=addeds[0], amount=amounts[1])
-    # AmountAdded.objects.create(id=4, added=addeds[1], amount=amounts[1])
-    # AmountAdded.objects.create(id=5, added=addeds[0], amount=amounts[2])
-    # AmountAdded.objects.create(id=6, added=addeds[1], amount=amounts[2])
-
-    # BaseOffer.objects.create(id=1, name='Base grande', available=True)
-    # BaseOffer.objects.create(id=2, name='Base mediana',available=True)
-    # BaseOffer.objects.create(id=3, name='Base pequeña',available=True)
+    @staticmethod
+    def poblate():
+        # Amount.objects.create(id=1, amount='Low')
+        # Amount.objects.create(id=2, amount='Meduim')
+        # Amount.objects.create(id=3, amount='High')
     
-    # added_list = AmountAdded.objects.all()
-    # bases = BaseOffer.objects.all()
-
-    # Offer.objects.create(id=1, base_offer=bases[0], amount_added=added_list[0])
-    # Offer.objects.create(id=2, base_offer=bases[0], amount_added=added_list[1])
-    # Offer.objects.create(id=3, base_offer=bases[0], amount_added=added_list[2])
-    # Offer.objects.create(id=4, base_offer=bases[0], amount_added=added_list[3])
-    # Offer.objects.create(id=5, base_offer=bases[0], amount_added=added_list[4])
-    # Offer.objects.create(id=6, base_offer=bases[0], amount_added=added_list[5])
-    # Offer.objects.create(id=7, base_offer=bases[1], amount_added=added_list[0])
-    # Offer.objects.create(id=8, base_offer=bases[1], amount_added=added_list[1])
-    # Offer.objects.create(id=9, base_offer=bases[1], amount_added=added_list[2])
-    # Offer.objects.create(id=10, base_offer=bases[1], amount_added=added_list[3])
-    # Offer.objects.create(id=11, base_offer=bases[1], amount_added=added_list[4])
-    # Offer.objects.create(id=12, base_offer=bases[1], amount_added=added_list[5])
-    # Offer.objects.create(id=13, base_offer=bases[2], amount_added=added_list[0])
-    # Offer.objects.create(id=14, base_offer=bases[2], amount_added=added_list[1])
-    # Offer.objects.create(id=15, base_offer=bases[2], amount_added=added_list[2])
-    # Offer.objects.create(id=16, base_offer=bases[2], amount_added=added_list[3])
-    # Offer.objects.create(id=17, base_offer=bases[2], amount_added=added_list[4])
-    # Offer.objects.create(id=18, base_offer=bases[2], amount_added=added_list[5])
-
-    # User.objects.create(username='juanki', first_name='juan', password='123')
-    # Group.objects.create()
-    pass
+        # Added.objects.create(id=1, name='queso')
+        # Added.objects.create(id=2, name='jamon')
+    
+        # addeds = Added.objects.all()
+        # amounts = Amount.objects.all()
+    
+        # AmountAdded.objects.create(id=1, added=addeds[0], amount=amounts[0])
+        # AmountAdded.objects.create(id=2, added=addeds[1], amount=amounts[0])
+        # AmountAdded.objects.create(id=3, added=addeds[0], amount=amounts[1])
+        # AmountAdded.objects.create(id=4, added=addeds[1], amount=amounts[1])
+        # AmountAdded.objects.create(id=5, added=addeds[0], amount=amounts[2])
+        # AmountAdded.objects.create(id=6, added=addeds[1], amount=amounts[2])
+    
+        # BaseOffer.objects.create(id=1, name='Base grande', available=True)
+        # BaseOffer.objects.create(id=2, name='Base mediana',available=True)
+        # BaseOffer.objects.create(id=3, name='Base pequeña',available=True)
+        
+        # added_list = AmountAdded.objects.all()
+        # bases = BaseOffer.objects.all()
+    
+        # Offer.objects.create(id=1, base_offer=bases[0], amount_added=added_list[0], price=100)
+        # Offer.objects.create(id=2, base_offer=bases[0], amount_added=added_list[1], price=100)
+        # Offer.objects.create(id=3, base_offer=bases[0], amount_added=added_list[2], price=100)
+        # Offer.objects.create(id=4, base_offer=bases[0], amount_added=added_list[3], price=100)
+        # Offer.objects.create(id=5, base_offer=bases[0], amount_added=added_list[4], price=100)
+        # Offer.objects.create(id=6, base_offer=bases[0], amount_added=added_list[5], price=100)
+        # Offer.objects.create(id=7, base_offer=bases[1], amount_added=added_list[0], price=100)
+        # Offer.objects.create(id=8, base_offer=bases[1], amount_added=added_list[1], price=100)
+        # Offer.objects.create(id=9, base_offer=bases[1], amount_added=added_list[2], price=100)
+        # Offer.objects.create(id=10, base_offer=bases[1], amount_added=added_list[3], price=100)
+        # Offer.objects.create(id=11, base_offer=bases[1], amount_added=added_list[4], price=100)
+        # Offer.objects.create(id=12, base_offer=bases[1], amount_added=added_list[5], price=100)
+        # Offer.objects.create(id=13, base_offer=bases[2], amount_added=added_list[0], price=100)
+        # Offer.objects.create(id=14, base_offer=bases[2], amount_added=added_list[1], price=100)
+        # Offer.objects.create(id=15, base_offer=bases[2], amount_added=added_list[2], price=100)
+        # Offer.objects.create(id=16, base_offer=bases[2], amount_added=added_list[3], price=100)
+        # Offer.objects.create(id=17, base_offer=bases[2], amount_added=added_list[4], price=100)
+        # Offer.objects.create(id=18, base_offer=bases[2], amount_added=added_list[5], price=100)
+    
+        pass
