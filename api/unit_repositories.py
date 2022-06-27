386643c0c57d1062c61e56b365d6f170d7230eac
IronBeardX <alejandroyeroval@gmail.com>
2022-06-27 18:39:45 -0400

    unit repositories implemented

--- api/unit_repositories.py
+++ api/unit_repositories.py
@@ -119,3 +119,122 @@ class OrderListRepo(object):
     def delete(self, by_id):
         self.__db_repo.delete(by_id)
 
+class ComplaintRepo(object):
+    def __init__(self, db_repo) -> None:
+        self.__db_repo = db_repo
+
+    def get_all(self):
+        complaint = self.__db_repo.get_all()
+        return complaint
+
+    def get_element(self, id):
+        return self.__db_repo.get_element(id)
+
+    def create(self, id, text, client):
+        return self.__db_repo.create(id, text, client)
+
+    def update(self, by_id, id, text, client):
+        return self.__db_repo.update(by_id, id, text, client)
+
+    def delete(self, by_id):
+        return self.__db_repo.delete(by_id)
+
+class GroupRepo(object):
+    def __init__(self, db_repo) -> None:
+        self.__db_repo = db_repo
+
+    def get_all(self):
+        complaint = self.__db_repo.get_all()
+        return complaint
+
+    def get_element(self, id):
+        return self.__db_repo.get_element(id)
+
+    def create(self, id, name, users, permissions):
+        return self.__db_repo.create(id, name, users, permissions)
+
+    def update(self, by_id, id, name, users, permissions):
+        return self.__db_repo.update(by_id, id, name, users, permissions)
+
+    def delete(self, by_id):
+        return self.__db_repo.delete(by_id)
+
+class PermisionRepo(object):
+    def __init__(self, db_repo) -> None:
+        self.__db_repo = db_repo
+
+    def get_all(self):
+        complaint = self.__db_repo.get_all()
+        return complaint
+
+    def get_element(self, id):
+        return self.__db_repo.get_element(id)
+
+    def create(self, id, code, name):
+        return self.__db_repo.create(id, code, name)
+
+    def update(self, by_id, id, code, name):
+        return self.__db_repo.update(by_id, id, code, name)
+
+    def delete(self, by_id):
+        return self.__db_repo.delete(by_id)
+
+class ProductRepo(object):
+    def __init__(self, db_repo) -> None:
+        self.__db_repo = db_repo
+
+    def get_all(self):
+        complaint = self.__db_repo.get_all()
+        return complaint
+
+    def get_element(self, id):
+        return self.__db_repo.get_element(id)
+
+    def create(self, id, name, amount):
+        return self.__db_repo.create(id, name, amount)
+
+    def update(self, by_id, id, name, amount):
+        return self.__db_repo.update(by_id, id, name, amount)
+
+    def delete(self, by_id):
+        return self.__db_repo.delete(by_id)
+
+class UpgradeStoreRepo(object):
+    def __init__(self, db_repo) -> None:
+        self.__db_repo = db_repo
+
+    def get_all(self):
+        complaint = self.__db_repo.get_all()
+        return complaint
+
+    def get_element(self, id):
+        return self.__db_repo.get_element(id)
+
+    def create(self, id, date, products):
+        return self.__db_repo.create(id, date, products)
+
+    def update(self, by_id, id, date, products):
+        return self.__db_repo.update(by_id, id, date, products)
+
+    def delete(self, by_id):
+        return self.__db_repo.delete(by_id)
+
+class UserRepo(object):
+    def __init__(self, db_repo) -> None:
+        self.__db_repo = db_repo
+
+    def get_all(self):
+        complaint = self.__db_repo.get_all()
+        return complaint
+
+    def get_element(self, id):
+        return self.__db_repo.get_element(id)
+
+    def create(self, id, username, password, is_admin):
+        return self.__db_repo.create(id, username, password, is_admin)
+
+    def update(self, by_id, id, username, password, is_admin):
+        return self.__db_repo.update(by_id, id, username, password, is_admin)
+
+    def delete(self, by_id):
+        return self.__db_repo.delete(by_id)
