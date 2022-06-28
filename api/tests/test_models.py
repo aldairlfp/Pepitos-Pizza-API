0450497f38177b07774e931b8236069ddeeaaeae
IronBeardX <alejandroyeroval@gmail.com>
2022-06-28 02:40:29 -0400

    BaseOfferTestsAlmostDone

---
+++ api/tests/test_models.py
@@ -0,0 +1,61 @@
+from pickle import TRUE
+from selectors import BaseSelector
+from django.test import TestCase
+from matplotlib.style import available
+
+from ..models import Added, BaseOffer
+
+
+class BaseOfferTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        ad1 = Added.objects.create(id = 1, name = "queso", price = 10)
+        ad2 = Added.objects.create(id = 2, available = False, name = "jamon", price = 15)
+        ad3 = Added.objects.create(id = 3, name = "cebolla", price = 7)
+
+        b1 = BaseOffer.objects.create(
+            id=1, name="pizza", price=10)
+        b1.addeds.set([ad1, ad2, ad3])
+        
+    def test_id_label(self):
+        base_offer = BaseOffer.objects.get(id=1)
+        field_label = base_offer._meta.get_field('id').verbose_name
+        self.assertEqual(field_label, 'id')
+
+    def test_name_label(self):
+        base_offer = BaseOffer.objects.get(id=1)
+        field_label = base_offer._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_available_label(self):
+        base_offer = BaseOffer.objects.get(id=1)
+        field_label = base_offer._meta.get_field('available').verbose_name
+        self.assertEqual(field_label, 'available')
+
+    def test_addeds_label(self):
+        base_offer = BaseOffer.objects.get(id=1)
+        field_label = base_offer._meta.get_field('addeds').verbose_name
+        self.assertEqual(field_label, 'addeds')
+
+    def test_price_label(self):
+        base_offer = BaseOffer.objects.get(id=1)
+        field_label = base_offer._meta.get_field('price').verbose_name
+        self.assertEqual(field_label, 'price')
+
+    def test_first_name_max_length(self):
+        base_offer = BaseOffer.objects.get(id=1)
+        max_length = base_offer._meta.get_field('name').max_length
+        self.assertEqual(max_length, 100)
+
+    def test_object_addeds_is_list_with_addeds(self):
+        base_offer = BaseOffer.objects.get(id = 1)
+        expected_object_name = f'id_BO: {base_offer.id}, name_BO: {base_offer.name}, addeds: {base_offer.addeds.filter(available=True)}'
+        self.assertEqual(str(base_offer), expected_object_name)
+
+    #TODO: Ver como se hace bien el request de los addeds
+    def test_objects_available_addeds_are_available(self):
+        base_offer = BaseOffer.objects.get(id = 1)
+        print(base_offer._meta.get_field('addeds'))
+        expected_addeds = [base_offer._meta.get_field('addeds')]
+        self.assertEqual(base_offer.available_addeds, expected_addeds)
