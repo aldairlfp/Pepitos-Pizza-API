878e79b55d4015bc2622cff9703efc88389f3207
aldairlfp <aldairalfonsoperez@gmail.com>
2022-06-29 07:06:40 -0700

    Test admin site

--- api/admin.py
+++ api/admin.py
@@ -4,13 +4,6 @@ from rest_framework.authtoken.admin import TokenAdmin
 
 from .models import *
 
-admin.site.register(BaseOffer)
-admin.site.register(Added)
-admin.site.register(RequestedOffer)
-admin.site.register(Client)
-admin.site.register(Order)
-admin.site.register(OrderList)
-
 TokenAdmin.raw_id_fields = ['user']
 
 # Register your models here.
