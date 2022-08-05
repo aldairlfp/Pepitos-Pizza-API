1510d2c1a70248bc0f3cb3074c324d8955ef81f9
aldairlfp <aldairalfonsoperez@gmail.com>
2022-08-05 03:23:51 +0100

    New way port

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('/documentation', app, document);
 
-  await app.listen(3000 || process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
